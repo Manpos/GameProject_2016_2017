9685280c646d0c7736671a117d65dd1394a2630b
Manpos <miquelpostigo@gmail.com>
2016-12-20 12:25:20 +0100

    Crop texture

--- src/Asteroids/GameScene.hh
+++ src/Asteroids/GameScene.hh
@@ -5,26 +5,35 @@
 #include "TimeManager.hh"
 
 class GameScene : public Scene {
+
+	struct EnemySpawnPos {
+		float x, y;
+	};
+
 private:
 	std::vector<Enemy> enemyVector;
 	int lifes, initEnemies, incremEnemiesNum;
 	float enemInitialSpeed;
 	int level;
-	struct EnemySpawnPos {
-		float x, y;
-	};
+
+	EnemySpawnPos spawn;
+	SDL_Rect spriteClips[11];
 
 	int once = 0;
+
 public:
 	void OnEntry();
 	void OnExit();
 	void Update();
 	void Draw();
+
 	GameScene(int lif, int iniEnem, int incEnem, float enemIniSpd ) 
 		: lifes(lif), initEnemies(iniEnem), incremEnemiesNum(incEnem), enemInitialSpeed(enemIniSpd){}
+
 	int EnemySpawnNumber() {
 		int totalEnemies;
 		totalEnemies = initEnemies + incremEnemiesNum*level;
 		return totalEnemies;
 	}
+
 };
