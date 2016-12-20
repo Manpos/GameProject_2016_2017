5362df9c2e9c86c7330aa66fd542ee0af4931642
mripoll <mripoll@PB1-17>
2016-12-20 13:52:27 +0100

    Classes

--- src/Asteroids/EnemyClass.hh
+++ src/Asteroids/EnemyClass.hh
@@ -1,13 +1,41 @@
 #pragma once
+#include <SDL.h>
+
 
 enum EnemyType { SMALL, MEDIUM, LARGE, S_OVNI, L_OVNI };
+
+struct position {
+	float x, y;
+};
+struct velocity {
+	float x, y;
+};
+
 class Enemy {
 private:
-	struct position{
-		float x, y;
-	};
+	position pos;
+	velocity vel;
 	EnemyType type;
 	SDL_Rect area;
 public:
-	Enemy(float x, float y, EnemyType ty);
+	Enemy(float x, float y, EnemyType ty) {
+		pos.x = x;
+		pos.y = y;
+		type = ty;
+	};
+
+	void Start() {
+
+	}
+
+	void Move() { // Com mes petit, mes velocitat
+		pos.x += vel.x;
+		pos.y += vel.y;
+	};
+
+	void Update() {
+
+	}
+
+	// Dispara en funció del tipus, S_OVNI dispara al jugador. L_OVNI aleatoriament
 };
--- src/Asteroids/PlayerClass.hh
+++ src/Asteroids/PlayerClass.hh
@@ -1,1 +1,34 @@
 #pragma once
+#include <SDL.h>
+#include "InputManager.hh"
+#include "Window.hh"
+
+
+struct position {
+	float x, y;
+};
+
+class Player {
+private:
+	position pos;
+	int life;
+	position referncePoint;
+	SDL_Rect area;
+public:
+	Player(float x, float y, int lif, float refX, float refY) {
+		pos.x = x;
+		pos.y = y;
+		life = lif;
+		referncePoint.x = refX;
+		referncePoint.y = refY;
+	};
+
+	void move() {
+		int mX = IM.GetMouseCoords().x;
+		int mY = IM.GetMouseCoords().y;
+	};
+	
+	void shoot() {
+		
+	}
+};
